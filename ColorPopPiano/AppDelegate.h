c6bb353e5904f438d769fa776c4d0c6c2291228a
Soojung Ha <soojungha@soojungha-macbookpro2.roam.corp.google.com>
2018-06-11 20:16:54 -0700

    Empty project

---
+++ ColorPopPiano/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColorPopPiano
+//
+//  Created by Soojung Ha on 6/11/18.
+//  Copyright © 2018 Soojung Ha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
